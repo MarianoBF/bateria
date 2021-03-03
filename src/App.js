03413af21d4b47ca4316dcddda8e18f24713761d
MarianoBF <mariano.b.fernandez@gmail.com>
2021-03-03 13:43:42 -0300

    destacar instrumento elegido

--- src/App.js
+++ src/App.js
@@ -11,10 +11,13 @@ import snare from "./components/audio/snare.wav";
 import tink from "./components/audio/tink.wav";
 import tom from "./components/audio/tom.wav";
 
-
-
 export class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      mostrar: "",
+    }
+  }
 
   componentDidMount() {
     document.addEventListener('keydown', this.reproducirSonidoLetra, false);
@@ -34,27 +37,27 @@ export class App extends React.Component {
     if (INSTRUMENTOS.includes(letra)) { 
     const instrumentoASonar = document.getElementById(`${letra}`);
     instrumentoASonar.play()
-    // this.setState({mostrar: !show })
-
+    this.setState({mostrar: letra })
+    setTimeout(()=>this.setState({mostrar: ""}),300)
     }
   }
 
-
   render() {
+
   return (
     <div id="drum-machine">
       <h1>Batería: hacé clic o tocá la tecla para hacer sonar el instrumento</h1>
 
       <div className="displayBateria" id="display">
-        <Instrumento codigo = "81" instrumento = "boom" letra = "Q" sonido = {boom} />
-        <Instrumento codigo = "87" instrumento = "clap" letra = "W" sonido = {clap} />
-        <Instrumento codigo = "69" instrumento = "hihat" letra = "E" sonido = {hihat} />
-        <Instrumento codigo = "65" instrumento = "kick" letra = "A" sonido = {kick} />
-        <Instrumento codigo = "83" instrumento = "openhat" letra = "S" sonido = {openhat} />
-        <Instrumento codigo = "68" instrumento = "ride" letra = "D" sonido = {ride} />
-        <Instrumento codigo = "90" instrumento = "snare" letra = "Z" sonido = {snare} />
-        <Instrumento codigo = "88" instrumento = "tink" letra = "X" sonido = {tink} />
-        <Instrumento codigo = "67" instrumento = "tom" letra = "C" sonido = {tom} />
+        <Instrumento codigo = "81" instrumento = "boom" letra = "Q" sonido = {boom} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "87" instrumento = "clap" letra = "W" sonido = {clap} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "69" instrumento = "hihat" letra = "E" sonido = {hihat} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "65" instrumento = "kick" letra = "A" sonido = {kick} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "83" instrumento = "openhat" letra = "S" sonido = {openhat} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "68" instrumento = "ride" letra = "D" sonido = {ride} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "90" instrumento = "snare" letra = "Z" sonido = {snare} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "88" instrumento = "tink" letra = "X" sonido = {tink} mostrar={this.state.mostrar} />
+        <Instrumento codigo = "67" instrumento = "tom" letra = "C" sonido = {tom} mostrar={this.state.mostrar} />
       </div>
     </div>
   );
--- src/components/Instrumento.js
+++ src/components/Instrumento.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 
 export default class Instrumento extends Component {
@@ -6,39 +5,29 @@ export default class Instrumento extends Component {
   constructor(props) {
     super(props);
     this.state = { letra: props.letra,
-    mostrar: false,
-    instrumento: props.instrumento,
-    codigo: +props.codigo };
+      mostrar: "",
+      instrumento: props.instrumento,
+      codigo: +props.codigo };
     this.reproducirSonido = this.reproducirSonido.bind(this);
-
-}
-
-
-
+  }
 
   reproducirSonido(j) {
-    console.log(j.type)
-    const show = this.state.mostrar;
     const letra = this.state.letra;
     const instrumentoASonar = document.getElementById(`${letra}`);
     instrumentoASonar.play()
-    this.setState({mostrar: !show })
+    this.setState({mostrar: letra })
+    setTimeout(()=>this.setState({mostrar: ""}),300)
   }
-
-  
-
-
 
   render() {
     return (
-      // onKeyDown={(e) => this.reproducirSonidoLetra(e)
-      <div onClick={(j) => this.reproducirSonido(j)}  id={this.props.instrumento} className="instrumento" tabIndex={0} >
+      <div onClick={(j) => this.reproducirSonido(j)}  id={this.props.instrumento} className={(this.props.mostrar === this.props.letra || this.state.mostrar === this.props.letra) ? "instrumentoDestacado" : "instrumento"} tabIndex={0} >
         <p className="letraInstrumento">{this.props.letra}</p>
-        {this.state.mostrar && <p>{this.props.instrumento}</p>}
+        <p>{this.props.instrumento}</p>
+        
         <audio id={this.props.letra} className="clip" src={this.props.sonido}>
         </audio>
       </div>
     );
-  }
-    
+  }   
 }
